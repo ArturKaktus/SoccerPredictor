9fa157cb23c707df9162a1836517091af0956413
ArturKaktus <arturkaktus1995@gmail.com>
2018-12-21 20:59:38 +0500

    Initial menu

---
+++ main.py
@@ -0,0 +1,18 @@
+print ("This program is version 0.01")
+print ("Autor is Artur Kaktus Milovidov")
+print ("github.com/ArturKaktus/SoccerPredictor")
+print ("\n\n\n")
+
+max_places = input("How many places in the standings? ")
+
+print ("\n1. Home")
+hm_place = input("Place in the standings ")
+hm_balls = input("How many balls scored? ")
+hm_balls_last = input("How many balls scored in the last 3 game? ")
+hm_games = input("How many games are played per season? ")
+
+print ("\n2. Guest")
+gst_place = input("Place in the standings ")
+gst_balls = input("How many balls scored? ")
+gst_balls_last = input("How many balls scored in the last 3 game? ")
+gst_games = input("How many games are played per season? ")
